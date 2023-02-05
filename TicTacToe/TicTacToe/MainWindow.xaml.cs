ba32532295afe0c9576723d0c29312bf4007bcc3
Vladislav <112644268+JPRemizov@users.noreply.github.com>
2023-02-05 21:26:55 +0300

    Update MainWindow.xaml.cs

--- TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace TicTacToe
 {
-    ///// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private BotLogic bot = new BotLogic();
